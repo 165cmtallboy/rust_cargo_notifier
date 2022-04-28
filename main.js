c655328614048eb3e4a545c304407bfc1226f45f
techan <nomeme106@gmail.com>
2022-04-28 21:51:55 +0900

    v2

--- main.js
+++ main.js
@@ -2,18 +2,23 @@ import axios from 'axios';
 import fs from 'fs';
 import server from '@liamcottle/rustplus.js';
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = 3030
-const d_end = 'https://discord.com/api/webhooks/964727188844851261/5KGJCbS7aSlmqDahNEMekBdRSp4WlueRl7Qfo9Ya-rRP3JQnuC-QcqYxqKaoMrUZV3d4';
-var s = new server('206.71.159.131', '28083', '76561198935889907', '-1181378574')
+const d_end = process.env.ENDPOINT;
+console.log(process.env.SECRET)
+var s = new server('206.71.159.131', '28083', '76561198935889907', process.env.SECRET)
 var current = 0;
-
+const NOSEND = true;
 
 async function sendMessage(body){
-    await axios.post(
-        d_end,
-        body)
+    if(!NOSEND){
+      await axios.post(
+          d_end,
+          body);
+    }
 }
 
 async function oneTerm() {
@@ -22,16 +27,26 @@ async function oneTerm() {
     console.log('searching...', s.websocket.readyState);
 
     var res = await new Promise((resolve) => s.getMapMarkers((res) => resolve(res)))
-    console.log(res.response.mapMarkers.markers)
+    
+    // team
+    s.getTeamInfo((message) => {
+        message.response.teamInfo.members
+          .forEach(({ name, x, y, spawnTime, deathTime}) => {
+              console.log(`${name}\t\t${x}\t${y}\t${spawnTime}\t${deathTime}`)
+          });
+    });
+
+    // cargo
     res.response.mapMarkers.markers.forEach(async (data) => {
+        console.log(data.type, data.x, data.y)
         if(data.type === 3){
-            console.log(data.sellOrders)
+            // console.log(data.sellOrders)
         }
-        console.info(`data found ${data.type}\t\t${data.id}`);
+        // console.info(`data found ${data.type}\t\t${data.id}`);
         if (data.type === 5 && data.id !== current) {
+            console.log("CARGO INCOMMING")
             current = data.id
-            await axios.post(
-                d_end,
+            sendMessage(
                 { content: 'カーゴきてるぞ！' }
             )
         }
@@ -39,38 +54,19 @@ async function oneTerm() {
 }
 
 s.on('connected', async () => {
-    s.getTeamInfo((message) => {
-        message.response.teamInfo.members
-          .forEach((member) => {
-              console.log(member.steamId);
-          });
-    });
     console.info('connected')
     // sendMessage({content: 'hello'});
-    // setInterval(oneTerm, 10 * 1000);
     s.getMap((res) => {
         fs.writeFile("public/map.jpg", res.response.map.jpgImage, (err) => {
             if(!err)
                 console.log("Map wrote.")
             else
                 console.error(err);
+            setInterval(oneTerm, 1 * 1000);
         })      
     })
 });
 
-s.on('message', (message) => {
-    console.log(message)
-    if(message.broadcast && message.broadcast.entityChanged){
-
-        var entityChanged = message.broadcast.entityChanged;
-
-        var entityId = entityChanged.entityId;
-        var value = entityChanged.payload.value;
-
-        console.log("entity " + entityId + " is now " + (value ? "active" : "inactive"));
-
-    }
-});
 s.connect();
 app.use(express.static('public'))
 app.listen(port, () => {
